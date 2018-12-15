28bd3628398a78502244a070cd6c5e71d6f58207
Nehal Nafady <nehalnafady631@gmail.com>
2018-12-15 17:37:16 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*global console, $*/
+$(function () {
+    'use strict';
+    var winH = $(window).height(),
+        upperH = $('.upper-bar').innerHeight(),
+        navH   = $('.navbar').innerHeight();
+    $('.slider, .carousel-item').height(winH - (upperH + navH));
+    
+    //featured work shuffle
+    $('.featured-work ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        if ($(this).data('class') === 'all') {
+            $('.shuffle-imgs .col-md').css('opacity', 1);
+        } else {
+            $('.shuffle-imgs .col-md').css('opacity', 0.5);
+            $($(this).data('class')).parent().css('opacity', 1);
+        }
+    });
+    
+});
